63329a441d8f0d28d485ef14c53948d2686b384d
Samuel <samnmbuguah@gmail.com>
2023-03-31 12:39:48 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+let reversedWord = word.split("").reverse().join("");
+if (word === reversedWord){
+  return true
+}
+else {
+  return false
+}
 }
 
 /* 
@@ -18,8 +24,19 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("phase"));
 }
 
 module.exports = isPalindrome;
