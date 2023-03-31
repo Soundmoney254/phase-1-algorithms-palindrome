4e0291e70ab8968222b6006ee2d255f3e7b0a146
Samuel <samnmbuguah@gmail.com>
2023-03-31 12:59:44 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -10,10 +10,21 @@ else {
 
 /* 
   Add your pseudocode here
+  Initialize reversedWord which represents the input parameter reveresed.
+  Split the word into an array of letters.
+  Reverse the array of letters output after the split.
+  Join the array of the reversed letters to form a word.
+  Compare the word and the reversed word to return true when they are equal and false otherwise
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here.
+  I initialized a variable called reversedWord and assigned it to the result of: 
+    Splitting the input word with no delimiter to output an array of letters.
+    Reversing the array of letters to form another array of the letters reversed.
+    Joining the array of letters reversed with no delimiter to form a string of the input word in reversed format.
+  Once the reversedWord variable contains a string that has gone through the above processes I used an if statement to check if the input ""word" is equal to the "reversedWord".
+    If they are equal the if statement returns true and else if they are not equal it returns false.
 */
 
 // You can run `node index.js` to view these console logs
